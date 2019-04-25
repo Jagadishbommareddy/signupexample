8d084d33563957f798f231fae780afbccdfd8c62
jagadish <jagadish6060@gmail.com>
2019-04-25 10:36:15 +0530

    create a model

---
+++ signup/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
+# Create your models here.
+
+class Signup(models.Model):
+    user = models.ForeignKey(User, null= True, on_delete=models.SET_NULL)
+    email = models.EmailField()
+    description = models.CharField(max_length= 255)
+
+admin.register(Signup)
